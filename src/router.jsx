ccd965b891e802c0d97e1f6be7103088efbcac7a
nicerjs23 <nicerjs23@gmail.com>
2024-09-14 03:28:34 +0900

    ✨Feat: 스플래시페이지 추가 완

--- src/router.jsx
+++ src/router.jsx
@@ -3,7 +3,7 @@ import App from "./App";
 import About from "./pages/About/index";
 import Home from "./pages/Home/index";
 import NotFound from "./pages/notFound";
-
+import Splash from "./pages/Splash/index";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -11,6 +11,8 @@ const router = createBrowserRouter([
     children: [
       // 홈 페이지
       { path: "/", element: <Home /> },
+      //스플래시 페이쥐
+      { path: "/splash", element: <Splash /> },
 
       // 어바웃 페이지
       { path: "/about", element: <About /> },
